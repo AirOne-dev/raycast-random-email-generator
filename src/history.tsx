2040ac5c5d6eafbe2019099b4998b43794585025
AirOne-dev <erwan1207@gmail.com>
2024-07-12 15:59:21 +0200

    use AppleScript to type the email because it allow to type in non copy/pasteable fields

--- src/history.tsx
+++ src/history.tsx
@@ -6,9 +6,21 @@ export default function Command() {
   const data: { email: string; datetime: string; id: number }[] = [];
 
   const [isLoading, setIsLoading] = useState(true);
-  const [showingDetail, setShowingDetail] = useState(false);
+  const [showingDetail] = useState(false);
   const [searchText, setSearchText] = useState("");
   const [filteredList, setFilteredList] = useState(data);
+
+  const clearStorage = async () => {
+    setIsLoading(true);
+    await LocalStorage.clear();
+    data.splice(0, data.length);
+    setFilteredList(data);
+    setIsLoading(false);
+  };
+
+  const getStorage = async () => {
+    return Object.entries(await LocalStorage.allItems());
+  };
 
   useEffect(() => {
     if (isLoading) {
@@ -16,7 +28,7 @@ export default function Command() {
         storage.forEach((item) => {
           if (item[0].startsWith("email-")) {
             const index = item[0].split("-")[1];
-            const datetime = new Date((storage.find((item) => item[0] === `datetime-${index}`) ?? [,])[1]);
+            const datetime = new Date((storage.find((item) => item[0] === `datetime-${index}`) ?? [null, null])[1]);
             data.push({
               id: parseInt(index),
               email: item[1],
@@ -29,7 +41,7 @@ export default function Command() {
         setIsLoading(false);
       });
     } else {
-      setFilteredList(data.filter(({ email }) => email.includes(searchText)));
+      setFilteredList(data.filter(({ email }) => email.includes(searchText)).sort((a, b) => b.id - a.id));
     }
   }, [searchText]);
 
@@ -40,21 +52,18 @@ export default function Command() {
       isLoading={isLoading}
       isShowingDetail={showingDetail}
     >
-      {filteredList.sort((a, b) => b.id - a.id).map((item, index) => (
+      {filteredList.map((item) => (
         <List.Item
           key={item.email}
           title={`${item.email} (${item.datetime})`}
           actions={
             <ActionPanel>
               <Action title="Select" onAction={() => pasteSelectedEmail(item.email)} />
+              <Action title="Clear History" onAction={() => clearStorage()} />
             </ActionPanel>
           }
         />
       ))}
     </List>
   );
-}
-
-async function getStorage() {
-  return Object.entries(await LocalStorage.allItems());
 }
--- src/random.tsx
+++ src/random.tsx
@@ -1,32 +1,45 @@
-import { Clipboard, getPreferenceValues, LocalStorage, LaunchProps, showHUD } from "@raycast/api";
-import { fakerFR as faker } from '@faker-js/faker';
+import { getPreferenceValues, LocalStorage, LaunchProps, showHUD, closeMainWindow } from "@raycast/api";
+import { fakerFR as faker } from "@faker-js/faker";
+import { runAppleScript } from "@raycast/utils";
+
 interface RandomArgument {
   type?: string;
 }
+
 export default async function Command(props: LaunchProps<{ arguments: RandomArgument }>) {
   const preferences = getPreferenceValues();
-  const currentGender = props.arguments?.type === 'f' ? 'female' : 'male';
+  const currentGender = props.arguments?.type === "f" ? "female" : "male";
 
   const firstname = faker.person.firstName(currentGender).toLowerCase();
   const lastname = faker.person.lastName(currentGender).toLowerCase();
-  const adjective = faker.word.adjective().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  const adjective = faker.word
+    .adjective()
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
 
   const removeAccents = (str: string): string => {
-    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    return str
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(" ", "");
   };
 
-  const email = removeAccents((() => {
-    switch (Math.floor(Math.random() * 4)) {
-      case 0:
-        return `${firstname}.${lastname}`;
-      case 1:
-        return `${lastname}.${firstname}`;
-      case 2:
-        return `${firstname}.${adjective}`;
-      default:
-        return `${lastname}.${adjective}`;
-    }
-  })()) + `@${preferences.email_domain}`;
+  const email =
+    removeAccents(
+      (() => {
+        switch (Math.floor(Math.random() * 4)) {
+          case 0:
+            return `${firstname}.${lastname}`;
+          case 1:
+            return `${lastname}.${firstname}`;
+          case 2:
+            return `${firstname}.${adjective}`;
+          default:
+            return `${lastname}.${adjective}`;
+        }
+      })(),
+    ) + `@${preferences.email_domain}`;
 
   const datetime = new Date().toISOString();
 
@@ -34,10 +47,17 @@ export default async function Command(props: LaunchProps<{ arguments: RandomArgu
   await LocalStorage.setItem(`email-${nbItems}`, email);
   await LocalStorage.setItem(`datetime-${nbItems}`, datetime);
 
-  pasteSelectedEmail(email);
+  await pasteSelectedEmail(email);
 }
 
-export function pasteSelectedEmail(email: string) {
-  Clipboard.paste(email);
-  showHUD(`${email} pasted`);
+export async function pasteSelectedEmail(email: string) {
+  await closeMainWindow({ clearRootSearch: true });
+
+  try {
+    // we use AppleScript to type the email because it allow to type in non copy/pasteable fields
+    await runAppleScript(`tell application "System Events" to keystroke "${email}"`);
+    await showHUD(`${email} pasted`);
+  } catch (error: any) {
+    await showHUD(`Failed to paste email: ${error.message}`);
+  }
 }
